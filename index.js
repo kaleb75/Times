388119b60921eaa4a80e208ac5cac2641f861870
Kaleb Chavira <44348427+kaleb75@users.noreply.github.com>
2025-06-21 20:57:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Timestamp Microservice. Use /api/:date or /api for current time.");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/api", (req, res) => {
